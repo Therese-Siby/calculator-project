aae189542a6df24afd85187b886bd1126564bf8a
Therese-Siby <71522481+Therese-Siby@users.noreply.github.com>
2024-11-18 21:20:06 +0530

    Add calculator project files

---
+++ main.js
@@ -0,0 +1,18 @@
+// function to display content 
+
+function displayContent(content){
+    result.value += content
+}
+// clear input
+function calcClear(){
+    result.value =""
+}
+// valuate  expressions
+function calcOutput() {
+    result.value =eval(result.value)
+    
+}
+// backspace
+function removeLastDigit() {
+    result.value = result.value.slice(0,-1);
+}
